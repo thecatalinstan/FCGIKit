22048356021d45e534cbc22aeff8f99677e97e83
Cătălin Stan <catalin.stan@me.com>
2014-10-15 20:54:28 +0200

    Framework

--- FCGIKit/FCGIKit.h
+++ FCGIKit/FCGIKit.h
@@ -2,27 +2,34 @@
 //  FCGIKit.h
 //  FCGIKit
 //
-//  Created by Cătălin Stan on 4/27/13.
-//  Copyright (c) 2013 Catalin Stan. All rights reserved.
+//  Created by Cătălin Stan on 15/10/14.
+//  Copyright (c) 2014 Catalin Stan. All rights reserved.
 //
 
-#import "FKConstants.h"
+//! Project version number for FCGIKit.
+FOUNDATION_EXPORT double FCGIKitVersionNumber;
 
-#import "AsyncSocket.h"
-#import "FCGIRequest.h"
+//! Project version string for FCGIKit.
+FOUNDATION_EXPORT const unsigned char FCGIKitVersionString[];
 
-#import "NSString+FCGIKit.h"
-#import "NSHTTPCookie+FCGIKit.h"
-#import "NSDate+RFC1123.h"
+#import <FCGIKit/FKConstants.h>
 
-#import "FKApplicationDelegate.h"
-#import "FKApplication.h"
-#import "FKHTTPRequest.h"
-#import "FKHTTPResponse.h"
-#import "FKBackgroundThread.h"
+#import <FCGIKit/AsyncSocket.h>
+#import <FCGIKit/FCGIRequest.h>
 
-#import "FKNib.h"
-#import "FKView.h"
-#import "FKViewController.h"
-#import "FKRoute.h"
-#import "FKRoutingCenter.h"
+#import <FCGIKit/NSString+FCGIKit.h>
+#import <FCGIKit/NSHTTPCookie+FCGIKit.h>
+#import <FCGIKit/NSDate+RFC1123.h>
+
+#import <FCGIKit/FKApplicationDelegate.h>
+#import <FCGIKit/FKApplication.h>
+#import <FCGIKit/FKHTTPRequest.h>
+#import <FCGIKit/FKHTTPResponse.h>
+#import <FCGIKit/FKBackgroundThread.h>
+
+#import <FCGIKit/FKNib.h>
+#import <FCGIKit/FKView.h>
+#import <FCGIKit/FKViewController.h>
+#import <FCGIKit/FKRoute.h>
+#import <FCGIKit/FKRoutingCenter.h>
+
